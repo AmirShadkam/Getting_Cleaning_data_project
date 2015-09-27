83ea0254ce891af1a4a4dbf1ce090499417a8710
AmirShadkam <a.shadkam@gmail.com>
2015-09-27 16:48:03 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,7 +4,8 @@
 #  4-  Appropriately labels the data set with descriptive variable names. 
 #  5-  From the data set in step 4, creates a second, independent tidy data set with the average 
 #      of each variable for each activity and each subject.
-
+#######################
+# the data should be unzipped first
 
 ## read the train and test data
 data_train_X <-read.table("UCI HAR Dataset/train/X_train.txt")
